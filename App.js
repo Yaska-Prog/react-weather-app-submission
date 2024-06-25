40913ed2da02a7d5c8c94a990c762424872ffa20
Yaska-Prog <christianleon442@gmail.com>
2024-06-25 22:37:11 +0700

    Initial commit

---
+++ App.js
@@ -0,0 +1,65 @@
+// Import axios, BASE_URL, dan API_KEY
+import React, { useState } from 'react'
+import axios from 'axios'
+import { BASE_URL, API_KEY } from './src/constant'
+import { View, StyleSheet } from 'react-native'
+import WeatherSearch from './src/components/weatherSearch'
+import WeatherInfo from './src/components/weatherInfo'
+
+const App = () => {
+  // Definisikan state "weatherData" dan "setWeatherData"
+  const [weatherData, setWeatherData] = useState();
+  const [status, setStatus] = useState('');
+  const renderComponent = () => {
+    switch (status) {
+      case 'loading':
+        return <ActivityIndicator size="large" />
+      case 'success':
+        return <WeatherInfo weatherData={weatherData} />
+      case 'error':
+        return (
+          <Text>
+            Something went wrong. Please try again with a correct city name.
+          </Text>
+        )
+      default:
+        return
+    }
+  }
+  const searchWeather = (location) => {
+    // Mengatur status ke "loading"
+    setStatus('loading')
+    axios
+      .get(`${BASE_URL}?q=${location}&appid=${API_KEY}`)
+      .then((response) => {
+        const data = response.data
+        data.visibility /= 1000
+        data.visibility = data.visibility.toFixed(2)
+        data.main.temp -= 273.15
+        data.main.temp = data.main.temp.toFixed(2)
+        setWeatherData(data)
+        // Mengatur status ke "success"
+        setStatus('success')
+      })
+      .catch((error) => {
+        // Mengatur status ke "error"
+        setStatus('error')
+      })
+  }
+
+  return (
+    <View style={styles.container}>
+      <WeatherSearch searchWeather={searchWeather} />
+      {/* Tampilkan data cuaca ketika ada weatherData */}
+      {weatherData && <WeatherInfo weatherData={weatherData} />}
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+  },
+})
+
+export default App
